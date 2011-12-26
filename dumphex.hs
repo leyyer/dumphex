d8ffa44f0ad806e4c56cb33e325cd0555b33be27
leyyer <sry@tianyisc.com>
2011-12-26 10:40:22 +0800

    reindent

--- dumphex.hs
+++ dumphex.hs
@@ -10,16 +10,18 @@ import Text.Printf (printf)
 main :: IO ()
 main = do
   (f:t:_) <- getArgs
-  liftM2 (,) (openFile f ReadMode) (openFile t WriteMode) >>= dumpOut_ >>= \(f,t) -> hClose f >> hClose t
+  liftM2 (,) (openFile f ReadMode) (openFile t WriteMode) 
+	>>= dumpOut 
+	>>= \(f,t) -> hClose f >> hClose t
     
-dumpOut_ :: (Handle, Handle) -> IO (Handle, Handle)
-dumpOut_ (f, t) = hPutStrLn t "const unsigned char data[] = {" >> dumpOut f t >> hPutStrLn t "};" >> return (f, t)
-  
 {- | dump to output file, input->output->IO () -}
-dumpOut :: Handle -> Handle -> IO ()
-dumpOut from to = do
-  chunk <- hGetNonBlocking from 16
-  let s = "\t" ++ (concatMap (printf "0x%02x," . fromEnum) $ unpack chunk)
-  hIsEOF from >>= \eof -> case eof of
-    True -> hPutStrLn to $ init s
-    _    -> hPutStrLn to s >> dumpOut from to
+dumpOut :: (Handle, Handle) -> IO (Handle, Handle)
+dumpOut (f, t) = hPutStrLn t "const unsigned char data[] = {" 
+	>> dumpOut' f t >> hPutStrLn t "};" 
+	>> return (f, t)
+	where dumpOut' from to = do
+			  chunk <- hGetNonBlocking from 16
+			  let s = "\t" ++ (concatMap (printf "0x%02x," . fromEnum) $ unpack chunk)
+			  hIsEOF from >>= \eof -> case eof of
+				  True -> hPutStrLn to $ init s
+				  _    -> hPutStrLn to s >> dumpOut' from to
